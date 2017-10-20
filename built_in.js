37b501a1757f001017996212319d5de28383e8b8
manishsaraan <manish021js@gmail.com>
2017-10-20 10:33:17 +0530

    initial push

---
+++ built_in.js
@@ -0,0 +1,8 @@
+var os  = require('os');
+var toMb = function(f){
+	 return (Math.round((f/1024/1024) * 100)) / 100;
+};
+
+console.log('host : ',os.hostname());
+console.log('15 min load average ',os.loadavg()[2]);
+console.log(toMb(os.freemem()) + " of " + toMb(os.totalmem()) + ' memory free!');
